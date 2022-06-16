aa7bcb98afea84acf24188d75b1be383bcd8551b
Delfino Farfan <delfino.farfan18@gmail.com>
2022-06-15 21:40:54 -0500

    footer update, form update, home update, css update

--- components/Nav.js
+++ components/Nav.js
@@ -3,19 +3,9 @@ import html from "html-literal";
 export default links => html`
   <nav>
     <i class="fa fa-window-close"></i>
-      ${links.map(
-        link =>
-          `<li><a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a></li>`
-      )}
-    </ul>
+    ${links.map(
+      link =>
+        `<li><a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a></li>`
+    )}
   </nav>
 `;
-{
-  /* <li class="nav-img"> */
-}
-{
-  /* <a href="/Home"> */
-}
-// <img src="${logo}" />
-// </a>
-// </li>
--- components/views/Testimonials.js
+++ components/views/Testimonials.js
@@ -1,12 +1,16 @@
 import html from "html-literal";
 
-export default state => html`
-  <p class="text-box">
-    "This product has allowed me to spend more time with my family, and I'm
-    still able to compete within my league" - Cam H.
-  </p>
-  <p class="text-box">
-    "I can't <b>BELIEVE</b> how much information I was missing out on! FFF has
-    allowed me to become more competitive than ever!" - Jeff G.
-  </p>
+export default () => html`
+  <div id="text-box-1">
+    <p>
+      "This product has allowed me to spend more time with my family, and I'm
+      still able to compete within my league" - Cam H.
+    </p>
+  </div>
+  <div id="text-box-2">
+    <p>
+      "I can't BELIEVE how much information I was missing out on! FFF has
+      allowed me to become more competitive than ever!" - Jeff G.
+    </p>
+  </div>
 `;
